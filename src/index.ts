1ea8ba19d8aea637d0253db2239c8a1ef8200e2d
Shane Liu <shane1997liu@gmail.com>
2022-03-28 23:17:29 -0700

    updating console log to match example output (#2)

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,9 @@ import { calculateBalance } from './functions/calculateBalance';
 const printDailyBalance = async (): Promise<Map<String, Number>> => {
     const db: dailyBalanceInterface[] = await fetchDB();
     const dailyBalanceSheet: Map<String, Number> = calculateBalance(db);
-    console.log(dailyBalanceSheet);
+    dailyBalanceSheet.forEach( (value, key) => {
+        console.log(`${key} ${value}`)
+    })
     return dailyBalanceSheet;
 }
 
